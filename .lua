05b6419b51943c9c378c776d59186cc41f0b461a
BadTime <96690318+idkfffff@users.noreply.github.com>
2022-07-22 18:34:11 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,4 @@
+	local A_1 = "\224\180\184\224\180\184\224\181\141\224\180\168\224\181\135\224\180\185\224\180\130"
+local item = "Torch"
+local bread = -1000000
+game.ReplicatedStorage.rems.shop:FireServer("tools", item, bread, A_1)
